0563007b502837722c7e219374d0f5b83e228a47
gdog2u <gdog2u@gmail.com>
2014-10-30 17:36:31 -0400

    Added comments to clarify methods.

--- Earnest/src/game/Container.java
+++ Earnest/src/game/Container.java
@@ -25,27 +25,19 @@ public class Container {
 	 * @param Player's type
 	 */
 	public void genInv(int level, int luck, Item.poolType type){
+		System.out.println("made it here");
 		Random r = new Random();
 		int items = r.nextInt(level + 3);
 		for(int i = 0; i < items; i++){
 			inventory.add(Item.itemPool(type));
 		}
+		System.out.println("done. Items created: " + inventory.size());
 	}
 	
 	/**
-	 * @deprecated
-	 * Old version. Used to return chest contents as a string.
-	 * Replaced with {@link open()} Method
+	 * Randomly creates a title for the container.
+	 * @return title
 	 */
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < inventory.size(); i++){
-			sb.append(inventory.get(i).getName() + "\n");
-		}
-		return sb.toString();
-		
-	}
-	
 	public String getContainerTitle(){
 		Random r = new Random();
 		int chance = r.nextInt(10);
@@ -60,11 +52,18 @@ public class Container {
 		}
 	}
 	
+	/**
+	 * Opens the {@link FrameContainer} gui for the container.
+	 */
 	public void open(){
 		FrameContainer chest = new FrameContainer(getContainerTitle(), inventory);
 		chest.setVisible(true);
 	}
 
+	/**
+	 * 
+	 * @return the contents of the container.
+	 */
 	public ArrayList<Item> getContents() {
 		return inventory;
 	}
