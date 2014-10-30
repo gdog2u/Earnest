5967293b9efed88984ddfc4185e7092d71eb038d
gdog2u <gdog2u@gmail.com>
2014-10-30 17:32:44 -0400

    Beginning work on the dungeon's room System. Added boolean hasEnemies. Added method for generating chests in the room.

--- Earnest/src/game/Room.java
+++ Earnest/src/game/Room.java
@@ -1,6 +1,7 @@
 package game;
 
 import game.Frames.FrameMainGame;
+import game.Items.Item;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -14,19 +15,33 @@ public class Room {
 	
 	Random r = new Random();
 	
-	ArrayList<Container> containersList = new ArrayList<>();
+	private ArrayList<Container> containersList = new ArrayList<>();
+	private boolean hasEnemies = false;
 	
 	public Room(){
 		genChests();
+		System.out.println("done");
 	}
 	
 	public void genChests(){
-		for(int i = 0; i < 3; i++){
+		for(int i = 0; i < 1; i++){
 			int chance = r.nextInt(25);
 			if(chance <= FrameMainGame.getHero().getLuck()){
 				containersList.add(new Container());
+					containersList.get(containersList.size()-1).genInv(FrameMainGame.getHero().getLevel(), FrameMainGame.getHero().getLuck(), Item.randomPool());
+					containersList.get(containersList.size()-1).getContents();
+			}else{
+				System.out.println("failed");
 			}
 		}
+	}
+
+	public boolean getHasEnemies() {
+		return hasEnemies;
+	}
+
+	public void setHasEnemies(boolean hasEnemies) {
+		this.hasEnemies = hasEnemies;
 	}
 	
 }
