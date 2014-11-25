57816a34b3aa25649a6412bd3009d2928a738d26
gdog2u <gdog2u@gmail.com>
2014-11-24 19:38:46 -0500

    Added get methods for Enemy. Really messing up the commits.

--- src/main/java/game/Enemy.java
+++ src/main/java/game/Enemy.java
@@ -9,4 +9,12 @@ public abstract class Enemy {
 	private double health;
 	private double dmg;
 	private ArrayList<Item> loot = new ArrayList<>();
+	
+	public double getHealth(){
+		return health;
+	}
+	
+	public double getDamage(){
+		return dmg;
+	}
 }
