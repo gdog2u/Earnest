20e11cfcc35a4c793d1b85fdd8aa49fcf0c5111a
gdog2u <gdog2u@gmail.com>
2014-12-07 02:41:29 -0500

    Added ability to add color to the labels. May be used in weapon condition, or rarity later.

--- src/main/java/game/WrappingLabel.java
+++ src/main/java/game/WrappingLabel.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.HashMap;
+
 import javax.swing.JLabel;
 
 /**
@@ -13,10 +15,28 @@ public class WrappingLabel extends JLabel {
 	 * 
 	 */
 	private static final long serialVersionUID = 5876653286734873774L;
+	
+	/**
+	 * @param name
+	 * @param hexvalue
+	 */
+	private HashMap<String, String> colors = new HashMap<>();
 
 	public WrappingLabel(String text) {
 		super("<html>" + text + "</br></html>");
 		// TODO Auto-generated constructor stub
+	}
+	
+	public WrappingLabel(String text, String color) {
+		init();
+		setText("<html><font color=\"" + colors.get(color.toLowerCase()) + "\">" + text + "</br></font></html>");
+	}
+	
+	public void init(){
+		colors.put("red", "#FF0000");
+		colors.put("blue", "#0000FF");
+		colors.put("green", "#00FF00");
+		
 	}
 
 }
